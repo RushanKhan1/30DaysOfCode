f64ba103dbf499aa94e8afbb84eb3c252346287a
Rushan Khan <rushan.khan2000@gmail.com>
2020-05-30 23:25:21 +0530

    Day 0.

---
+++ Day0.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    cout << "Hello, World." << endl;
+    cout << s << endl;
+    return 0;
+}
