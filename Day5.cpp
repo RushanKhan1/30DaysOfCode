3b6d6df0dcf240469eaf158c716a3a0b9e3314d1
Rushan Khan <rushan.khan2000@gmail.com>
2020-06-04 16:33:30 +0530

    Day 5.

---
+++ Day5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1; i <= 10; i++)
+    {
+        cout << n << " x " << i << " = " << n*i << endl;
+    }
+}
